49bfb4edb66aee836b9bb94ca5079cfda5951f59
Uma Sahni <monkeytown64@gmail.com>
2024-02-13 10:57:47 +0530

    Products page updated

--- fronted/Products/backpack.js
+++ fronted/Products/backpack.js
@@ -3,18 +3,35 @@ const getProducts = () => {
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
-      const products = data.produts.products
-      showProductsToDOM(products)
+      const products = data.produts[0].products;
+      console.log(products);
+      showProductsToDOM(products);
     })
     .catch((err) => console.log(err));
 };
 
-getProducts()
+getProducts();
 
-function showProductsToDOM(products){
-    return (`
-    
-    
-    
-    `)
+function showProductsToDOM(products) {
+  let container = document.getElementById("products");
+  let productsHTML = "";
+
+  products.forEach((product) => {
+    productsHTML += `
+      <div class="child">
+        <img src=https://images.bewakoof.com/t640/${product.display_image} height="300px" alt="">
+        <p class="font-size-small-dark">${product.brand}</p>
+        <div class="flex-inside-child-div font-size-small">
+          <p>${product.name}</p>
+          <i class="fa-regular fa-heart fa-xl"></i>
+        </div>
+        <div class="flex-inside-child-div">
+          <p>₹ ${product.price}</p>
+          <p>${product.member_discount}</p>
+        </div>
+      </div>
+    `;
+  });
+
+  container.innerHTML = productsHTML;
 }
